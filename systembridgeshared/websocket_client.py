ce0edabaa81d5ca20bf991980e56a764b4d5004c
Aidan Timson <aidan@timmo.dev>
2023-12-17 17:47:34 +0000

    Fix

--- systembridgeshared/websocket_client.py
+++ systembridgeshared/websocket_client.py
@@ -105,7 +105,7 @@ class WebSocketClient(Base):
 
         future: asyncio.Future[Response] = asyncio.get_running_loop().create_future()
         self._responses[request.id] = future, response_type
-        await self._websocket.send_json(request)
+        await self._websocket.send_json(asdict(request))
         self._logger.debug("Sent message: %s", request)
 
         if wait_for_response:
