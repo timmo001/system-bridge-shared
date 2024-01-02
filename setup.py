1fb7c5fd6e894fa85137b0723283089d25cf1dba
Aidan Timson <aidan@timmo.dev>
2024-01-02 10:29:50 +0000

    Update deploy.yml and setup.py

--- setup.py
+++ setup.py
@@ -9,16 +9,22 @@ with open("requirements_setup.txt", encoding="utf-8") as f:
 with open("requirements.txt", encoding="utf-8") as f:
     requirements = f.read().splitlines()
 
+with open("README.md", encoding="utf-8") as f:
+    readme = f.read()
+
 setup(
     name="systembridgeshared",
-    description="System Bridge Shared",
-    keywords="system-bridge",
     author="Aidan Timson (Timmo)",
     author_email="aidan@timmo.dev",
+    description="System Bridge Shared",
+    keywords="system-bridge",
     license="Apache-2.0",
+    long_description=readme,
+    long_description_content_type="text/markdown",
     url="https://github.com/timmo001/system-bridge-shared",
-    packages=find_packages(exclude=["tests", "generator"]),
     install_requires=requirements,
+    packages=find_packages(exclude=["tests", "generator"]),
+    python_requires=">=3.11",
     setup_requires=requirements_setup,
     use_incremental=True,
 )
